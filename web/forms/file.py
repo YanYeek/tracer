ed3ea6a41eda35c50340bb18148b68e5b1d58e7b
YanYeek <48875922+YanYeek@users.noreply.github.com>
2020-09-11 17:17:16 +0800

    complete day11 edit file

--- web/forms/file.py
+++ web/forms/file.py
@@ -7,6 +7,7 @@
 @desc:
 '''
 from django import forms
+from django.core.exceptions import ValidationError
 
 from web import models
 from web.forms.bootstrap import BootStrapForm
@@ -16,3 +17,21 @@ class FolderModelForm(BootStrapForm, forms.ModelForm):
 	class Meta:
 		model = models.FileRepository
 		fields = ['name', ]
+
+	def __init__(self, request, parent_object, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.request = request
+		self.parent_object = parent_object
+
+	def clean_name(self):
+		name = self.cleaned_data.get('name')
+		# 数据库判断 当前目录下 此文件夹是否已存在
+		queryset = models.FileRepository.objects.filter(file_type=2, name=name, project=self.request.tracer.project)
+		if self.parent_object:
+			exists = queryset.filter(parent=self.parent_object).exists()
+
+		else:
+			exists = queryset.filter(parent__isnull=True).exists()
+		if exists:
+			raise ValidationError('文件夹已存在')
+		return name
--- web/views/file.py
+++ web/views/file.py
@@ -8,6 +8,7 @@
 '''
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms import model_to_dict
 
 from web.forms.file import FolderModelForm
 from web import models
@@ -17,17 +18,51 @@ from web import models
 # http://127.0.0.1:8000/manage/6/file/?folder=9
 def file(request, project_id):
 	""" 文件列表 & 添加文件夹 """
-	if request.method == "GET":
-		form = FolderModelForm()
-		return render(request, 'file.html', {'form': form})
-
 	parent_object = None
 	folder_id = request.GET.get('folder', "")
 	if folder_id.isdecimal():
 		parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
 		                                                     project=request.tracer.project).first()
-	# 添加文件夹
-	form = FolderModelForm(request.POST)
+	# GET 查看页面
+	if request.method == "GET":
+
+		breadcrumb_list = []
+		parent = parent_object
+		while parent:
+			# breadcrumb_list.insert(0, {'id': parent.id, 'name': parent.name})
+			breadcrumb_list.insert(0, model_to_dict(parent, ['id', 'name']))
+			parent = parent.parent
+
+		# 把当前目录所有的文件 & 文件夹获取到即可
+		queryset = models.FileRepository.objects.filter(project=request.tracer.project)
+		if parent_object:
+			# 进入了某一个目录
+			file_object_list = queryset.filter(parent=parent_object).order_by('-file_type')
+		else:
+			# 根目录
+			file_object_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+
+		form = FolderModelForm(request, parent_object)
+		content = {
+			'form': form,
+			'file_object_list': file_object_list,
+			'breadcrumb_list': breadcrumb_list
+		}
+		return render(request, 'file.html', content)
+
+	# POST 添加文件夹 & 修改文件夹
+	fid = request.POST.get('fid', '')
+	edit_object = None
+	if fid.isdecimal():
+		# 修改
+		edit_object = models.FileRepository.objects.filter(id=int(fid), file_type=2,
+		                                                   project=request.tracer.project).first()
+	if edit_object:
+		form = FolderModelForm(request, parent_object, data=request.POST, instance=edit_object)
+
+	else:
+		form = FolderModelForm(request, parent_object, data=request.POST)
+
 	if form.is_valid():
 		form.instance.project = request.tracer.project
 		form.instance.file_type = 2
