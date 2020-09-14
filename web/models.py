c55c633c0727aa9c1d2d74fdb5bedab6658f2c72
YanYeek <48875922+YanYeek@users.noreply.github.com>
2020-09-14 21:45:48 +0800

    complete Issue table structure design

--- web/models.py
+++ web/models.py
@@ -134,3 +134,71 @@ class FileRepository(models.Model):
 
 	update_user = models.ForeignKey(verbose_name='最近更新者', to='UserInfo')
 	update_datetime = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+
+class Issues(models.Model):
+	""" 问题 """
+	project = models.ForeignKey(verbose_name='项目', to='Project')
+	issues_type = models.ForeignKey(verbose_name='问题类型', to='IssuesType')
+	module = models.ForeignKey(verbose_name='模块', to='Module', null=True, blank=True)
+
+	subject = models.CharField(verbose_name='主题', max_length=80)
+	desc = models.TextField(verbose_name='问题描述')
+	priority_choices = (
+		("danger", "高"),
+		("warning", "中"),
+		("success", "低"),
+	)
+	priority = models.CharField(verbose_name='优先级', max_length=12, choices=priority_choices, default='danger')
+
+	# 新建、处理中、已解决、已忽略、待反馈、已关闭、重新打开
+	status_choices = (
+		(1, '新建'),
+		(2, '处理中'),
+		(3, '已解决'),
+		(4, '已忽略'),
+		(5, '待反馈'),
+		(6, '已关闭'),
+		(7, '重新打开'),
+	)
+	status = models.SmallIntegerField(verbose_name='状态', choices=status_choices, default=1)
+
+	assign = models.ForeignKey(verbose_name='指派', to='UserInfo', related_name='task', null=True, blank=True)
+	attention = models.ManyToManyField(verbose_name='关注者', to='UserInfo', related_name='observe', blank=True)
+
+	start_date = models.DateField(verbose_name='开始时间', null=True, blank=True)
+	end_date = models.DateField(verbose_name='结束时间', null=True, blank=True)
+	mode_choices = (
+		(1, '公开模式'),
+		(2, '隐私模式'),
+	)
+	mode = models.SmallIntegerField(verbose_name='模式', choices=mode_choices, default=1)
+
+	parent = models.ForeignKey(verbose_name='父问题', to='self', related_name='child', null=True, blank=True,
+	                           on_delete=models.SET_NULL)
+
+	creator = models.ForeignKey(verbose_name='创建者', to='UserInfo', related_name='create_problems')
+
+	create_datetime = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+	latest_update_datetime = models.DateTimeField(verbose_name='最后更新时间', auto_now=True)
+
+	def __str__(self):
+		return self.subject
+
+
+class Module(models.Model):
+	""" 模块（里程碑）"""
+	project = models.ForeignKey(verbose_name='项目', to='Project')
+	title = models.CharField(verbose_name='模块名称', max_length=32)
+
+	def __str__(self):
+		return self.title
+
+
+class IssuesType(models.Model):
+	""" 问题类型 例如：任务、功能、Bug """
+	title = models.CharField(verbose_name='类型名称', max_length=32)
+	project = models.ForeignKey(verbose_name='项目', to='Project')
+
+	def __str__(self):
+		return self.title
